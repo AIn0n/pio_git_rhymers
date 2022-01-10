577b97e582a64db6cf1f89f7a982b32e5f457f9a
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-01-10 02:59:54 +0100

    1.5 - testy, diagramy, dokumentacja - done

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,10 +1,20 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * class contains simple linked list implementation working with integers values
+ */
 public class IntLinkedList {
 
+    /**
+     * Default return value if list is empty
+     */
     public static final int IF_EMPTY = -1;
     Node last;
 
+    /**
+     * add element into list as last one.
+     * @param i value of new element
+     */
     public void push(int i) {
         if (last == null)
             last = new Node(i);
@@ -15,20 +25,33 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     * @return Does list contain any element?
+     */
     public boolean isEmpty() {
         return last == null;
     }
 
+    /**
+     * @return return true if list is full
+     * TODO: implement functionality
+     */
     public boolean isFull() {
         return false;
     }
 
+    /**
+     * @return return last element value
+     */
     public int top() {
         if (isEmpty())
             return IF_EMPTY;
         return last.value;
     }
 
+    /**
+     * @return return value and remove last element
+     */
     public int pop() {
         if (isEmpty())
             return IF_EMPTY;
